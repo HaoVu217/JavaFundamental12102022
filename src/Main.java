08819305d3d15cb1edb1e141cbe60e3abad943fc
HaoKiet <kiet21795@gmail.com>
2022-10-19 19:07:05 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello world!");
+
+        // 1: Kiểu dữ liệu
+        // - Nguyên thủy (Primitive)
+        //   -Ex: char, int, long, float, double, boolean,..
+        // - Đối tượng (Object)
+        //   -Ex: String, Integer, Long, Float, Double, Boolean
+
+        // 2: Khai báo biến
+        char world = 'a'; // char: Kí tự.
+        int number1 = -1; // int: Số nguyên
+        float number2 = 1.2f; // float: Kiểu thập phân, thêm chữ f để xác định.
+        boolean isOnline = true; // boolean: Chỉ có 2 giá trị True/False
+        
+    }
+}
