00acd0a8b6c4b0d7e9892c4a1307b4baecf994a5
HaoKiet <kiet21795@gmail.com>
2022-10-22 15:08:57 +0700

    toan tu

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,44 @@ public class Main {
         //   -Ex: String, Integer, Long, Float, Double, Boolean
 
         // 2: Khai báo biến
-        char world = 'a'; // char: Kí tự.
-        int number1 = -1; // int: Số nguyên
-        float number2 = 1.2f; // float: Kiểu thập phân, thêm chữ f để xác định.
-        boolean isOnline = true; // boolean: Chỉ có 2 giá trị True/False
-        
+//        char world = 'a'; // char: Kí tự.
+//        int number1 = -1; // int: Số nguyên
+//        float number2 = 1.2f; // float: Kiểu thập phân, thêm chữ f để xác định.
+//        boolean isOnline = true; // boolean: Chỉ có 2 giá trị True/False
+
+        // Tên, tuổi, sở thích, chiều cao.
+
+//        String name = "Hào Kiệt";
+//        int age = 27;
+//        String hobby = "Nghe nhac";
+//        Float height = 1.7f;
+//
+//        System.out.println(name);
+//        System.out.println(age);
+//        System.out.println(hobby);
+//        System.out.println(height);
+
+        // 3: Toán tử
+
+//        int a = 10;
+//        int b = 5;
+//        int result = b % a;
+//        // Chia lấy quần nguyên / , chia lấy phần thập phân % , lấy tất cả => ép kiểu
+//
+//        System.out.println(result);
+
+        int a = 10;
+//        a = a + 1; // tăng a lên 1 giá trị
+//        a += 1;
+//        a++;
+//        ++a;
+
+        int b = a++;
+        // a++ (gán giá trị , sau đó tăng 1 đơn vị)
+        // ++a (tăng 1 đơn vị , sau đó gán giá trị
+
+        System.out.println(b);
+
+
     }
 }
